933bd4efbefbf9e521c6b3726fdf17917268d0e7
md uddin <mduddin1998@yahoo.com>
2020-02-20 18:55:46 -0500

    firebase key added

---
+++ keys/FirebaseConfig.js
@@ -0,0 +1,17 @@
+import "firebase/auth";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyDw7x-B3jEw4eSX3ynSDfOm_usWPLqPmeQ",
+  authDomain: "studentcenter-813c8.firebaseapp.com",
+  databaseURL: "https://studentcenter-813c8.firebaseio.com",
+  projectId: "studentcenter-813c8",
+  storageBucket: "studentcenter-813c8.appspot.com",
+  messagingSenderId: "364020918570",
+  appId: "1:364020918570:web:eceb1c4e85e06bb572d653",
+  measurementId: "G-5XGMQRCPYT"
+};
+// Initialize Firebase
+var firebase = firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+export default firebase;
