8cc4094644d38b1b35a4744da9fb0a0569cf6283
Wei Lin <tonzr3@yahoo.com>
2020-02-20 17:23:28 -0500

    complete login and registration page

--- src/pages/login-page.component.js
+++ src/pages/login-page.component.js
@@ -1,72 +1,73 @@
 import React, { Component } from 'react';
 
 export default class LoginPage extends Component {
+  constructor(props) {
+    super(props);
 
-    constructor(props) {
-        super(props);
-    
-        this.onEmail = this.onEmail.bind(this);
-        this.onPassword = this.onPassword.bind(this);
-        this.onSubmit = this.onSubmit.bind(this);
-
-        this.state = {
-            email: '',
-            password: ''
-        }
+    this.state = {
+        email: '',
+        password: ''
     }
+  
+  this.onInputChange = this.onInputChange.bind(this);
+  this.onSubmit = this.onSubmit.bind(this);
+  }
 
-    onEmail(e) {
-        this.setState({
-          email: e.target.value
-        })
-      }
+  onInputChange(e){
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
 
-    onPassword(e) {
-        this.setState({
-          password: e.target.value
-        })
-      }
+  onSubmit(e) {
+      e.preventDefault(); 
 
-    onSubmit(e) {
-        e.preventDefault(); 
-
-        const LoginInfo = {
-            email: this.state.email,
-            password: this.state.password
-          }
-      
-        console.log(LoginInfo);
-
-        window.location = '/';
+      const LoginInfo = {
+          email: this.state.email,
+          password: this.state.password
         }
+    
+      console.log(LoginInfo);
 
+      window.location = '/';
+      }
 
+//textbox for user/password input
   render() {
     return (
         <div>
         <h3>Login</h3>
         <form onSubmit={this.onSubmit}>
+
           <div className="form-group"> 
             <label>Email: </label>
-            <input  type="text"
+            <input  type="email"
                 required
+                autoFocus
+                name="email"
                 className="form-control"
+                placeholder="Enter your email"
                 value={this.state.email}
-                onChange={this.onEmail}
+                onChange={this.onInputChange}
                 />
           </div>
+
           <div className="form-group"> 
             <label>Password: </label>
             <input  type="password"
                 required
+                name="password"
                 className="form-control"
+                placeholder="Enter your password"
                 value={this.state.password}
-                onChange={this.onPassword}
+                onChange={this.onInputChange}
                 />
           </div>
+
           <div className="form-group">
             <input type="submit" value="Confirm" className="btn btn-primary" />
           </div>
+          
         </form>
         </div>
     )
